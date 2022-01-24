deb766844d0359f77fdd1527f008e0e722988c28
Demonchuwu <cnm3575@utulsa.edu>
2022-01-24 15:10:57 -0600

    Updated unity
    Added cube prefab, beginning script, and basics of unity set up done.

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubeScript.cs
@@ -0,0 +1,26 @@
+/***
+* Created by Cristian Misla
+* Date Created: 1/24/2022
+*
+* Last Edited By Cristian Misla
+* Last Edited: 1/24/2022
+* 
+* Description: 
+***/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubeScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
