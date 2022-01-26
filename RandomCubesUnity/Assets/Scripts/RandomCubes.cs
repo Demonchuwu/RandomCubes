46934b852f8f3d55ec07ff53de309b478f25ae9b
Demonchuwu <cnm3575@utulsa.edu>
2022-01-26 15:18:03 -0600

    Update RandomCubes.cs
    added onto the code and updated the description to the top.
    -Created spawning system
    -Random transformation
    -Scaling and deleting

--- RandomCubesUnity/Assets/Scripts/RandomCubes.cs
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -5,7 +5,7 @@
 * Last Edited By: Cristian Misla
 * Last Edited: 1/26/2022
 * 
-* Description: 
+* Description: This code is to spawn the cube in random directions at 0,0,0 while it scales until it deletes itself. 
 ***/
 
 using System.Collections;
@@ -21,11 +21,36 @@ public class RandomCubes : MonoBehaviour
 
     void Start()
     {
-
+        gameObjectList = new List<GameObject>(); // Instatates the list
     }
 
     void Update()
     {
+        numberOfCubes++; //add to the number of cubes
+        GameObject gObj = Instantiate<GameObject>(cubePrefab); //creates cube
+        gObj.name = "Cube" + numberOfCubes; //name of cube instance
+        Vector3 insideUnitSphere = Random.insideUnitSphere;
+        gObj.transform.position = insideUnitSphere; //random location within a sphere radius of 1 out from 0,0,0
+        gameObjectList.Add(gObj); //add to list
+        List<GameObject> removeList = new List<GameObject>();//remove to list
 
+        foreach(GameObject goTemp in gameObjectList)
+        {
+            float scale = goTemp.transform.localScale.x; //records current scale
+            scale *= scalingFactor; //scale multiplied by scaling factor
+            goTemp.transform.localScale = Vector3.one * scale; //transform scale
+            if(scale <= 0.1f)
+            {
+                removeList.Add(goTemp);
+            }//end if(scale <= 0.1f)
+        }//end foreach(GameObject goTemp in gameObjectList)
+
+        foreach(GameObject goTemp in removeList)
+        {
+            gameObjectList.Remove(goTemp); //remove from game object list
+            Destroy(goTemp); //destory game object list
+            
+        }//end foreach(GameObject goTemp in removeList)
+        //Debug.Log(removeList.Count); //debugs the remove list
     }
 }
