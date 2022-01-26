4930085ba9cbc038a1060973846e87cbf7ca6dd1
Demonchuwu <cnm3575@utulsa.edu>
2022-01-26 14:06:53 -0600

    Update RandomCubeScript.cs
    Fixed and updated the details part of the code.

--- RandomCubesUnity/Assets/Scripts/RandomCubeScript.cs
+++ RandomCubesUnity/Assets/Scripts/RandomCubeScript.cs
@@ -1,12 +1,13 @@
 /***
-* Created by Cristian Misla
+* Created by: Cristian Misla
 * Date Created: 1/24/2022
 *
-* Last Edited By Cristian Misla
-* Last Edited: 1/24/2022
+* Last Edited By: Cristian Misla
+* Last Edited: 1/26/2022
 * 
 * Description: 
 ***/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
